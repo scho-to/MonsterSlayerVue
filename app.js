4ce7642dd7da0dbd5ad321bc73c343ec10e66cd0
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2020-11-27 13:09:05 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const getDmg = (max, min) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0
+    };
+  },
+  computed: {
+    monsterBarStyles(){
+      return {width: this.monsterHealth + '%'};
+    },
+    playerBarStyles() {
+      return {width: this.playerHealth + '%'};
+    },
+    specialAttackLimiter() {
+      return this.currentRound % 3 !== 0;
+    }
+  },
+  methods: {
+    attackMonster() {
+      this._doDmg(12, 5);
+    },
+    attackPlayer() {
+      this.playerHealth -= getDmg(15, 8);
+    },
+    specialAttackMonster() {
+      this._doDmg(25, 10);
+    },
+
+    _doDmg(max, min){
+      this.currentRound++;
+      this.monsterHealth -= getDmg(max, min);
+      this.attackPlayer();
+    }
+  }
+}).mount("#game");
